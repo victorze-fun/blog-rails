02f17482b1862781ec3441aed56553205ba23949
Victor <victorze@protonmail.com>
2022-09-28 17:14:36 -0500

    Add security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "victorze", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
